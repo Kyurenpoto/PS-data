565936e943308d3e397b47859a74896623a4f575
Kyurenpoto <heal9179@gmail.com>
2021-09-02 10:06:22 +0900

    Fix missed braces

--- string/suffix_array.h
+++ string/suffix_array.h
@@ -44,7 +44,7 @@ struct SuffixArray
     {
         const int n = origin.length();
         std::vector<int> rank(n * 2), nextRank(n * 2), index(n);
-        CountingSorter sorter(std::max(std::numeric_limits<char>::max(), n) + 1);
+        CountingSorter sorter(std::max((int)std::numeric_limits<char>::max(), n) + 1);
 
         arr.resize(n);
         std::iota(arr.begin(), arr.end(), 0);
@@ -52,12 +52,12 @@ struct SuffixArray
 
         for (int d = 1; d < n && rank[arr[n - 1]] != n; d *= 2)
         {
-            sorter.sort(rank.begin + d, rank.begin + (n + d), index,[](int i) { return i; });
-            sorter.sort(rank.begin, rank.begin + n, arr,[&index](int i) { return index[i]; });
+            sorter.sort(rank.begin() + d, rank.begin() + (n + d), index,[](int i) { return i; });
+            sorter.sort(rank.begin(), rank.begin() + n, arr,[&index](int i) { return index[i]; });
 
             nextRank[arr[0]] = 1;
 
-            auto cmp = [&d](int i, int j)
+            auto cmp = [&d, &rank](int i, int j)
             {
                 return rank[i] == rank[j] ? rank[i + d] < rank[j + d] : rank[i] < rank[j];
             };
@@ -65,7 +65,7 @@ struct SuffixArray
                 nextRank[arr[i]] =
                 nextRank[arr[i - 1]] + (cmp(arr[i - 1], arr[i]) ? 1 : 0);
 
-            std::copy_n(nextRank.begin(), n, rank.begin);
+            std::copy_n(nextRank.begin(), n, rank.begin());
         }
     }
 };
