e3ad790f62a0dedea1df2f1542b288fd7b5a199d
Kyurenpoto <heal9179@gmail.com>
2021-09-03 03:49:17 +0900

    Extract class 'SuffixArrayFactory'

--- string/suffix_array.h
+++ string/suffix_array.h
@@ -6,14 +6,44 @@
 
 struct SuffixArray
 {
-    const std::string origin;
     std::vector<int> arr;
+
+    SuffixArray(const std::vector<int>& arr) :
+        arr(arr)
+    {}
+
+    auto lowerbound(const std::string& origin, const std::string& pattern) const
+    {
+        return std::lower_bound(arr.begin(), arr.end(), pattern,
+            [&origin](const int& idx, const std::string& x)
+            {
+                return origin.substr(idx, x.length()) < x;
+            });
+    }
+
+    auto upperbound(const std::string& origin, const std::string& pattern) const
+    {
+        return std::upper_bound(arr.begin(), arr.end(), pattern,
+            [&origin](const std::string& x, const int& idx)
+            {
+                return x < origin.substr(idx, x.length());
+            });
+    }
+};
+
+struct SuffixArrayFactory
+{
+    std::string origin;
+
+    SuffixArrayFactory(const std::string& origin) :
+        origin(origin)
+    {}
 
     struct CountingSorter
     {
         std::vector<int> count;
 
-        CountingSorter(int n) :
+        CountingSorter(size_t n) :
             count(n)
         {}
 
@@ -26,46 +56,105 @@ struct SuffixArray
         )
         {
             std::fill(count.begin(), count.end(), 0);
-            
-            int n = end - begin;
+
+            size_t n = end - begin;
             for (int i = 0; i < n; ++i)
                 ++count[*(begin + i)];
 
             for (int i = 1; i < count.size(); ++i)
                 count[i] += count[i - 1];
 
-            for (int i = n - 1; i >= 0; --1)
-                index[--count[*(begin + i)] = indexer(i);
+            for (int i = n - 1; i >= 0; --i)
+                index[--count[*(begin + indexer(i))]] = indexer(i);
         }
     };
 
-    SuffixArray(const std::string& origin) :
-        origin(origin)
+    struct DoubleCountingSorter
     {
-        const int n = origin.length();
-        std::vector<int> rank(n * 2), nextRank(n * 2), index(n);
-        CountingSorter sorter(std::max((int)std::numeric_limits<char>::max(), n) + 1);
+        std::vector<int> index;
+        CountingSorter sorter;
 
-        arr.resize(n);
-        std::iota(arr.begin(), arr.end(), 0);
-        rank.insert(rank.begin(), origin.begin(), origin.end());
+        DoubleCountingSorter(size_t size) :
+            index(size),
+            sorter(std::max((size_t)std::numeric_limits<char>::max(), size) + 1)
+        {}
 
-        for (int d = 1; d < n && rank[arr[n - 1]] != n; d *= 2)
+        void sort(
+            std::vector<int>& arr,
+            std::vector<int>::iterator begin,
+            std::vector<int>::iterator end,
+            int d
+        )
         {
-            sorter.sort(rank.begin() + d, rank.begin() + (n + d), index,[](int i) { return i; });
-            sorter.sort(rank.begin(), rank.begin() + n, arr,[&index](int i) { return index[i]; });
+            sorter.sort(begin + d, end + d, index, [](int i) { return i; });
+            sorter.sort(begin, end, arr, [this](int i) { return index[i]; });
+        }
+    };
 
+    struct SuffixRank
+    {
+        size_t meaningfulSize;
+        std::vector<int> rank, nextRank;
+
+        SuffixRank(size_t size) :
+            meaningfulSize(size),
+            rank(size * 2),
+            nextRank(size * 2)
+        {}
+
+        void init(const std::string& origin)
+        {
+            std::copy(origin.begin(), origin.end(), rank.begin());
+        }
+
+        void update(const std::vector<int>& arr, size_t d)
+        {
             nextRank[arr[0]] = 1;
 
-            auto cmp = [&d, &rank](int i, int j)
+            auto cmp = [&d, this](int i, int j)
             {
                 return rank[i] == rank[j] ? rank[i + d] < rank[j + d] : rank[i] < rank[j];
             };
-            for (int i = 1; i < n; ++i)
+            for (int i = 1; i < meaningfulSize; ++i)
                 nextRank[arr[i]] =
                 nextRank[arr[i - 1]] + (cmp(arr[i - 1], arr[i]) ? 1 : 0);
 
-            std::copy_n(nextRank.begin(), n, rank.begin());
+            std::copy_n(nextRank.begin(), meaningfulSize, rank.begin());
         }
+
+        std::vector<int>::iterator begin()
+        {
+            return rank.begin();
+        }
+
+        std::vector<int>::iterator end()
+        {
+            return rank.begin() + meaningfulSize;
+        }
+
+        bool updatable(const std::vector<int>& arr) const
+        {
+            return rank[arr[meaningfulSize - 1]] != meaningfulSize;
+        }
+    };
+
+    SuffixArray create()
+    {
+        const size_t n = origin.length();
+        std::vector<int> arr(n);
+        DoubleCountingSorter sorter(n);
+        SuffixRank rank(n);
+
+        std::iota(arr.begin(), arr.end(), 0);
+        rank.init(origin);
+
+        for (size_t d = 1; d < n && rank.updatable(arr); d *= 2)
+        {
+            sorter.sort(arr, rank.begin(), rank.end(), d);
+
+            rank.update(arr, d);
+        }
+
+        return SuffixArray(arr);
     }
 };
